78a65ba9aeefbc8e5e55e1bbd68a5fe06f488b14
Saranraj <saran@atatus.com>
2021-05-11 14:32:31 +0530

    change composer find

--- bin/util/atatus.sh
+++ bin/util/atatus.sh
@@ -5,6 +5,7 @@ install_atatus_ext() {
     # otherwise users would have to have it in their require section, which is annoying in development environments
     ATATUS_LICENSE_KEY=${ATATUS_LICENSE_KEY:-}
     if [[ -n "$ATATUS_LICENSE_KEY" ]] && ! php -n $(which composer) show -d "$build_dir/.heroku/php" --installed --quiet heroku-sys/ext-atatus 2>/dev/null; then
+        echo "$ATATUS_LICENSE_KEY"
         if php -n $(which composer) require --update-no-dev -d "$build_dir/.heroku/php" -- "heroku-sys/ext-atatus:*" >> $build_dir/.heroku/php/install.log 2>&1; then
             echo "- Atatus detected, installed ext-atatus" | indent
         else
